67aee05872f7e1dfa778c7fd54eb0e2c7675683e
Soufian EL MEFTOUHI <shtpro3@gmail.com>
2018-07-22 19:52:24 +0100

    feat:Add Crud of product
    -Add product
    -Edit product by id
    -Delete product
    -List Products

--- src/app/admin/products/products-routing.module.ts
+++ src/app/admin/products/products-routing.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductsComponent } from './products.component';
+import {NewProductComponent} from "./new-product/new-product.component";
+import {EditProductComponent} from "./edit-product/edit-product.component";
 
 
 const routes: Routes = [
     {
       path: '',
       component: ProductsComponent
+    },
+    {
+      path: 'new',
+      component: NewProductComponent
+    }, {
+      path: 'edit/:id',
+      component: EditProductComponent
     }
   ];
 
--- src/app/admin/products/store/products.actions.ts
+++ src/app/admin/products/store/products.actions.ts
@@ -4,10 +4,9 @@ export const INCREMENT_NUMBER='INCREMENT_NUMBER'
 
 export class IncrementNumberOfProductes implements Action{
    readonly type=INCREMENT_NUMBER;
-   payload:number
 
    constructor(public payload:number) {}
-   
+
 }
 
 export type ProductActions=IncrementNumberOfProductes;
